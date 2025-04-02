aa98de4850982240beb4164dfac037f28209a5a1
khanmcodes <kkhalepoto@gmail.com>
2025-04-03 00:13:24 +0500

    update dashboard

--- app/(tabs)/dashboard.tsx
+++ app/(tabs)/dashboard.tsx
@@ -4,7 +4,7 @@ export default function Dashboard() {
   return (
     <View className="flex-1 justify-center items-center bg-dark-300">
       <Text className="text-2xl font-medium text-primary">
-        Dashboard Username
+        Dashboard
       </Text>
     </View>
   );
